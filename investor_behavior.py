697690004c27abeaf5a494f4d2a13dd7b326a727
Tim Smyrnov <smirnovtimophey@gmail.com>
2025-08-26 13:28:29 -0400

    Minor changes

--- investor_behavior.py
+++ investor_behavior.py
@@ -3,19 +3,13 @@ import time
 from orders import Order
 
 def order(symbol: str, data: dict) -> Order:
-    mid_price = data[symbol]
-    volatility = 0.50
     min_qty = 100
     max_qty = 1000
 
     side = random.choice(['BUY', 'SELL'])
-
-    fluct = random.uniform(-volatility, volatility)
-    price = round(mid_price + fluct, 2)
-
     qty = random.randint(min_qty, max_qty)
 
-    return Order(side, price, qty, symbol, 'MARKET', 'indv')
+    return Order(side, symbol, order_type='MARKET', qty=qty)
 
 if __name__ == '__main__':
     print(order('MSFT', {'AAPL': 101, 'MSFT': 201}))
